e9e0b600267b9cd3e4fa8de26ac7d71e9a941d7c
Thomas Kremer <tbone7243@gmail.com>
2021-04-06 13:09:47 -0400

    Revamped site, changed over from responsive sidebar to responsive header.  Added flexbox and an h2 header.  Also added a hamburger menu and a toggle method/button.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -59,3 +59,16 @@ function start() {
 }
 
 start();
+/* Peferred method with JS Event Listener */
+const toggleButton = document.getElementById('toggle-menu');
+const naviList = document.getElementById('navi-list');
+toggleButton.addEventListener('click', () => {
+     naviList.classList.toggle('active');
+})
+
+/* HTML Event Method */
+
+/* function toggleMenu() {
+     const naviList = document.getElementById('navi-list');
+     naviList.classList.toggle('active');
+} */
